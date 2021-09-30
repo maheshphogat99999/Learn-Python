7682a86959cda6eda15ea4370387bfa42b1ef97b
Mahesh <89020639+maheshphogat99999@users.noreply.github.com>
2021-09-30 23:03:02 +0530

    Add files via upload
    This is the very primary Task to check the output of the code. With the attached file Learn how to print in IDLE Shell to check the output. Same can be done in easier way in jyupter notebook as well.

---
+++ PRINT PRACTICE.py
@@ -0,0 +1,16 @@
+# START TYPING BELOW CODE. IT IS AS SIMPLE AS THAT.
+# HERE I HAVE WRITTEN IT IN 3.9.5 IDLE SHELL
+
+# IMPORTANT NOTE:- CODES WILL BE EXECUTED LINE AFTER LINE. SO UNDERSTAND THE IMPORTANCE OF LINE AND SPACES
+
+# BASIC STEPS AFTER TYPING ANY CODE
+# STEP-1 :- After typing code save it before clicking Run. So press ctrl+S or click on top right corner "File" option, there also save option is there
+# STEP -2 :- To see the output, either click "RUN" on the top or press "Fn+F5"
+
+print("Mahesh")
+print('Hi')
+print("Anand\nVidyanagar")   # \n WILL BEGIN CODE FROM NEXT LINE. IN OUTPUT 'Vidyanagar' will appear in new next line. Check it
+print("Gujarat" "\t" "India")   # \t WILL PROVIDE A SPACE BETWEEN TWO WORDS 
+print("Roses are Red" ,"\n" , end= "Sky is")  # This will print "Sky is" in a new line because \n is there and at the end of statement "blue" will be printed
+print("\t" "blue")  # Try to remove "end=" from above line and see the output. Blue will be printed in new a line below 
+
